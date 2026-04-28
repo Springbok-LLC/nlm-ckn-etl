07f44ae1a4b35722b778763216e5db4593cf4bfc
Raymond LeClair <raymond.leclair@gmail.com>
2026-04-28 12:34:08 -0400

    Update port handling to permit multiple ArangoDB instances

--- src/main/shell/stop-arangodb.sh
+++ src/main/shell/stop-arangodb.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
-container_id=$(docker ps -q -f "name=^arangodb$")
+port="${1:-8529}"
+if [[ "$port" == "8529" ]]; then
+    name="arangodb"
+else
+    name="arangodb-${port}"
+fi
+container_id=$(docker ps -q -f "name=^${name}$")
 if [ -n "$container_id" ]; then
    docker container stop "$container_id" > /dev/null
    docker container rm "$container_id" > /dev/null
